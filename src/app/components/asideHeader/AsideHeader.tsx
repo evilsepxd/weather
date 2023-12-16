f2bc5b6818e32c48bbc34750eec3896b540c7bb3
Peter Koval <evilsephy1@mail.ru>
2023-12-16 13:20:42 +0300

    приветствие зависит от времени

--- src/app/components/asideHeader/AsideHeader.tsx
+++ src/app/components/asideHeader/AsideHeader.tsx
@@ -15,12 +15,21 @@ function AsideHeader() {
 
 		return () => clearInterval(intervalId);
 	}, []);
-	
+
 	const formattedTime = time.toLocaleString('ru', { hour: '2-digit', minute: '2-digit' })
+
+	const greeting = () => {
+		const res = 'Good ';
+		const hours = time.getHours();
+		if (hours >= 6 && hours < 12) return res + 'Morning';
+		else if (hours >= 12 && hours < 18) return res + 'Afternoon';
+		else if (hours >= 18 && hours < 24) return res + 'Evening';
+		else return res + 'Night';
+	}
 
 	return (
 		<header className="aside-header">
-			<div className="aside-header__title">Good Evening</div>
+			<div className="aside-header__title">{ greeting() }</div>
 			<time className="aside-header__time">{ formattedTime }</time>
 		</header>
 	);
