f2e35914c99710d817d2996648736e0554dfee48
Peter Koval <evilsephy1@mail.ru>
2023-12-16 13:10:45 +0300

    часы отображают текущее время

--- src/app/components/asideHeader/AsideHeader.tsx
+++ src/app/components/asideHeader/AsideHeader.tsx
@@ -1,13 +1,27 @@
 
 
+import { useEffect, useState } from 'react';
+
 import './asideHeader.scss';
 
 function AsideHeader() {
 
+	const [time, setTime] = useState(new Date());
+
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setTime(new Date());
+		}, 1000);
+
+		return () => clearInterval(intervalId);
+	}, []);
+	
+	const formattedTime = time.toLocaleString('ru', { hour: '2-digit', minute: '2-digit' })
+
 	return (
 		<header className="aside-header">
 			<div className="aside-header__title">Good Evening</div>
-			<time className="aside-header__time">12:27 PM</time>
+			<time className="aside-header__time">{ formattedTime }</time>
 		</header>
 	);
 }
